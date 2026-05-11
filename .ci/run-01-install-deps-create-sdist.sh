a9c9b2efb2f41914ffc990826010e034194ae6b3
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-11 19:59:06 +0200

    Install dev-packages in nov20 branch

--- .ci/run-01-install-deps-create-sdist.sh
+++ .ci/run-01-install-deps-create-sdist.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 set -euxo pipefail
 . .ci/_source_common_env.sh
-if [ ! -d $PYTHONUSERBASE ]; then mkdir -p $PYTHONUSERBASE; fi
+#if [ ! -d $PYTHONUSERBASE ]; then mkdir -p $PYTHONUSERBASE; fi
 
-python -m pip install --cache-dir $CACHE_ROOT/pip_cache --upgrade-strategy=eager --upgrade cython
-python -m pip install --cache-dir $CACHE_ROOT/pip_cache -e .[all]
-python -c "import pycvodes; import pyodesys; import pygslodeiv2"  # debug this CI config
+PYTHON=python
+INSTALL_PIP_FLAGS="--cache-dir $CACHE_ROOT/pip_cache ${INSTALL_PIP_FLAGS:-}"  # --user
+for pypkg in pyodeint pygslodeiv2 pycompilation pycodeexport pycvodes pykinsol sym pyodesys; do
+    case $pypkg in
+        sym)
+            pypkg_fqn="git+https://github.com/bjodah/sym@jun21#egg=sym"
+            ;;
+        pyodeint)
+            pypkg_fqn="git+https://github.com/bjodah/pyodeint@sep21#egg=pyodeint"
+            ;;
+        pygslodeiv2)
+            pypkg_fqn="git+https://github.com/bjodah/pygslodeiv2@cython-except-plus#egg=pygslodeiv2"
+            ;;
+        pycompilation)
+            pypkg_fqn="git+https://github.com/bjodah/pycompilation@master#egg=pycompilation"
+            ;;
+        pycodeexport)
+            pypkg_fqn="git+https://github.com/bjodah/pycodeexport@qulify-extension-name-and-new-ci#egg=pycodeexport"
+            ;;
+        pycvodes)
+            pypkg_fqn="git+https://github.com/bjodah/pycvodes@may21#egg=pycvodes"
+            ;;
+        pyodesys)
+            pypkg_fqn="git+https://github.com/bjodah/pyodesys@bdf2#egg=pyodesys"
+            ;;
+        pykinsol)
+            pypkg_fqn="git+https://github.com/bjodah/pykinsol@jan25#egg=pykinsol"
+            ;;
+        *)
+            pypkg_fqn=$pypkg
+            ;;
+    esac
+    $PYTHON -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
+    #( cd /tmp; $PYTHON -m pytest -k "not pool_discontinuity_approx" --pyargs $pypkg )
+done
+
+$PYTHON -c "import pycvodes; import pyodesys; import pygslodeiv2"
+$PYTHON -m pip install $INSTALL_PIP_FLAGS -e .[all]
+[[ $(python3 setup.py --version) =~ ^[0-9]+.* ]]
 git fetch -tq
-cp -ra $(pwd) /dev/shm/
-cd /dev/shm/$(basename $(pwd))  # some issue with filesystem on CI server (race conditions)
-python -m build --sdist                   # test pip installable sdist (checks MANIFEST.in)
+cp -ra "$(pwd)" /dev/shm/          # some issue with filesystem on CI server (race conditions)
+cd "/dev/shm/$(basename $(pwd))"   # running in /dev/shm (RAM backed) seems to help.
+$PYTHON -m build --sdist                   # test pip installable sdist (checks MANIFEST.in)
 git archive -o dist/chempy-head.zip HEAD  # test pip installable zip (symlinks break)
 cd -
-cp -ra /dev/shm/$(basename $(pwd))/dist .
+cp -ra "/dev/shm/$(basename $(pwd))/dist" .
+rm -r "/dev/shm/$(basename $(pwd))"
